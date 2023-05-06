b7a5cff01332cf859e503e9a656ac5e7714f582e
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-05-05 19:58:56 -0500

    Format Python code using Shed.

--- other/scraper/scrape.py
+++ other/scraper/scrape.py
@@ -1,30 +1,34 @@
-import sys
 import json
-import time
-import urllib.request
 import re
+import sys
+import time
+
+import urllib2
 
 HOST = "http://api.steampowered.com"
 ENDPOINT = "IPublishedFileService/QueryFiles/v0001/"
 APPID = 4000
 NUMPERPAGE = 100
-DELAY = 0.1 # How long to delay between requests
+DELAY = 0.1  # How long to delay between requests
 FILENAME = "addons.txt"
 
 ignore_words = ["content", "server"]
-ignore_reg = "(?<!_){0}(?!_)" # Allow ignore words to be a part of the map name (surrounding underscores)
+ignore_reg = "(?<!_){0}(?!_)"  # Allow ignore words to be a part of the map name (surrounding underscores)
+
+
 def containsIgnoreWord(str, word):
     return re.search(ignore_reg.format(word), str) is not None
+
 
 def containsIgnoreWords(str):
     for word in ignore_words:
         if containsIgnoreWord(str, word):
             return True
-        
+
     return False
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("A Steam WebAPI key is required.")
         sys.exit(1)
@@ -39,15 +43,17 @@ if __name__ == "__main__":
     f = open(FILENAME, "w")
 
     while True:
-        req = "{0}/{1}?key={2}&appid={3}&requiredtags[0]=map&numperpage={4}&page={5}&return_metadata=1&query_type=1".format(HOST, ENDPOINT, key, APPID, NUMPERPAGE, page)
-        response = urllib.request.urlopen(req).read()
+        req = "{}/{}?key={}&appid={}&requiredtags[0]=map&numperpage={}&page={}&return_metadata=1&query_type=1".format(
+            HOST, ENDPOINT, key, APPID, NUMPERPAGE, page
+        )
+        response = urllib2.urlopen(req).read()
         resobj = json.loads(response.decode("utf-8", "ignore"))
         total = resobj["response"]["total"]
 
         for addon in resobj["response"]["publishedfiledetails"]:
             if "title" in addon and containsIgnoreWords(addon["title"]):
-                ign_str = u"Ignoring: " + addon["title"]
-                print(ign_str.encode('utf-8'))
+                ign_str = "Ignoring: " + addon["title"]
+                print(ign_str.encode("utf-8"))
                 continue
 
             # Add if not already in (sometimes query will give us dupes?)
@@ -57,25 +63,24 @@ if __name__ == "__main__":
 
         # Informative output
         finished = page * NUMPERPAGE + len(resobj["response"]["publishedfiledetails"])
-        print("Finished {0} addons. ({1:.2f}% of {2})".format(finished, finished * 100.0 / total, total))
+        print(
+            "Finished {} addons. ({:.2f}% of {})".format(
+                finished, finished * 100.0 / total, total
+            )
+        )
 
         # Move onto to the next page
         page += 1
 
         if page * NUMPERPAGE > resobj["response"]["total"]:
             break
-        else:   
+        else:
             # so valve doesn't get angry at us
             time.sleep(DELAY)
-    
-    # Results come back sorted, but reverse it so
-    # newer entries are added at the end instead of shifting everything at the beginning
-    workshopids.reverse()
 
-    print("Dumping {0} addons to {1}".format(len(workshopids), FILENAME))
+    print(f"Dumping {len(workshopids)} addons to {FILENAME}")
     for id in workshopids:
         f.write(id + "\n")
 
     print("Finished!!")
     f.close()
-
--- other/uploader/upload_multipart.py
+++ other/uploader/upload_multipart.py
@@ -1,195 +1,233 @@
-import fnmatch
-import sys
-import os
-import glob2
-import parser
-import tempfile
-import shutil
 import argparse
-from subprocess import check_output, CalledProcessError
+import os
+import parser
+import shutil
+import tempfile
+from subprocess import CalledProcessError, check_output
+
+import glob2
 
 gmod_dir = "F:/Steam Games/steamapps/common/GarrysMod/bin"
 
 gmad = os.path.join(gmod_dir, "gmad.exe")
 gmpublish = os.path.join(gmod_dir, "gmpublish.exe")
 
+
 def parseInt(s):
-	try:
-		return int(s)
-	except:
-		return None
+    try:
+        return int(s)
+    except:
+        return None
+
 
 def getFiles(path):
-	return glob2.glob(path, recursive = True)
+    return glob2.glob(path, recursive=True)
+
 
 def stripComments(line):
-	pos = line.find("#")
-	pos = pos if pos != -1 else len(line)
-	return line[:pos]
+    pos = line.find("#")
+    pos = pos if pos != -1 else len(line)
+    return line[:pos]
+
 
 def getPackSize(pack, filemap):
-	size = 0
-	for f, p in iter(filemap.items()):
-		if p != pack:
-			continue
+    size = 0
+    for f, p in iter(filemap.items()):
+        if p != pack:
+            continue
 
-		size += os.path.getsize(f)
+        size += os.path.getsize(f)
 
-	return size
+    return size
+
 
 def getAddonName(pack):
-	return os.path.splitext(pack)[0] + ".gma"
+    return os.path.splitext(pack)[0] + ".gma"
+
 
 def copyFile(src, dest):
-	path = os.path.dirname(dest)
-	if not os.path.exists(path):
-		os.makedirs(path)
+    path = os.path.dirname(dest)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-	shutil.copy(src, dest)
+    shutil.copy(src, dest)
 
 
-# incredibly lazy, sue me 
+# incredibly lazy, sue me
 # https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return f"{num:3.1f}{unit}{suffix}"
         num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
+    return "{:.1f}{}{}".format(num, "Yi", suffix)
+
 
 searchpath = ""
+
+
 def buildGMA(pack, filemap, tempdir):
-	print("Moving files...")
+    print("Moving files...")
 
-	# 1. Copy all the files over to the temp dir
-	for f, p in iter(filemap.items()):
-		if p != pack:
-			continue
-		relpath = os.path.join(tempdir, os.path.relpath(f, searchpath))
-		# os.makedirs(relpath)
+    # 1. Copy all the files over to the temp dir
+    for f, p in iter(filemap.items()):
+        if p != pack:
+            continue
+        relpath = os.path.join(tempdir, os.path.relpath(f, searchpath))
+        # os.makedirs(relpath)
 
-		copyFile(f, relpath)
+        copyFile(f, relpath)
 
-	# 2. Copy corresponding addon.txt 
-	print("Copying " + pack)
-	copyFile(pack, os.path.join(tempdir, "addon.json"))
+    # 2. Copy corresponding addon.txt
+    print("Copying " + pack)
+    copyFile(pack, os.path.join(tempdir, "addon.json"))
 
-	# 3 Invoke gmad and generate a new gma
-	gmaname = getAddonName(pack)
-	print("Building gma...")
-	try:
-		print(os.path.join(os.getcwd(), gmaname))
-		out = check_output([gmad, "create", "-folder", tempdir, "-out", os.path.join(os.getcwd(), gmaname) ], encoding='UTF-8')
-		print(out)
-	except CalledProcessError as e:
-		print("gmad FAILURE!!")
-		print(e.output)
-		raise
+    # 3 Invoke gmad and generate a new gma
+    gmaname = getAddonName(pack)
+    print("Building gma...")
+    try:
+        print(os.path.join(os.getcwd(), gmaname))
+        out = check_output(
+            [
+                gmad,
+                "create",
+                "-folder",
+                tempdir,
+                "-out",
+                os.path.join(os.getcwd(), gmaname),
+            ],
+            encoding="UTF-8",
+        )
+        print(out)
+    except CalledProcessError as e:
+        print("gmad FAILURE!!")
+        print(e.output)
+        raise
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Organize multi-part addon uploads")
-	parser.add_argument("dir", type=str, help="Root directory of addon to split up")
-	parser.add_argument("-c", "--changelog", dest="changelog", type=str, help="Text for what to put in the change log")
-	parser.add_argument("-b", "--build", dest="build", action="store_true", help="Enable building gmas")
-	parser.add_argument("-u", "--upload", dest="upload", action="store_true", help="Enable uploading of gmas to workshop")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Organize multi-part addon uploads")
+    parser.add_argument("dir", type=str, help="Root directory of addon to split up")
+    parser.add_argument(
+        "-c",
+        "--changelog",
+        dest="changelog",
+        type=str,
+        help="Text for what to put in the change log",
+    )
+    parser.add_argument(
+        "-b", "--build", dest="build", action="store_true", help="Enable building gmas"
+    )
+    parser.add_argument(
+        "-u",
+        "--upload",
+        dest="upload",
+        action="store_true",
+        help="Enable uploading of gmas to workshop",
+    )
+    args = parser.parse_args()
 
-	if args.dir != None:
-		searchpath = args.dir
+    if args.dir != None:
+        searchpath = args.dir
 
-	# Load in the split definition file
-	lines = None
-	with open("split.txt") as f:
-		lines = f.readlines()
+    # Load in the split definition file
+    lines = None
+    with open("split.txt") as f:
+        lines = f.readlines()
 
-	# Grab each definition and build a mapping for where each file will go
-	filemap = {}
-	curpack = None
-	allpacks = []
-	packids = {}
-	print("#### Beginning file scan")
-	for i, finfo in enumerate(lines):
+    # Grab each definition and build a mapping for where each file will go
+    filemap = {}
+    curpack = None
+    allpacks = []
+    packids = {}
+    print("#### Beginning file scan")
+    for i, finfo in enumerate(lines):
+        # Strip comments/whitespace
+        finfo = stripComments(finfo).strip()
 
-		# Strip comments/whitespace
-		finfo = stripComments(finfo).strip()
+        # Ignore blank lines
+        if len(finfo) == 0:
+            continue
 
-		#Ignore blank lines
-		if len(finfo) == 0:
-			continue
+        # Lines with ":" are addon pack directives
+        pack = finfo.split(":")
+        if len(pack) >= 2:
+            curpack = pack[0]
+            packids[curpack] = parseInt(pack[1])
 
-		#Lines with ":" are addon pack directives
-		pack = finfo.split(":")
-		if len(pack) >= 2:
-			curpack = pack[0]
-			packids[curpack] = parseInt(pack[1])
+            # Keep track of all packs we've come across
+            if curpack not in allpacks:
+                allpacks.append(curpack)
 
-			# Keep track of all packs we've come across
-			if curpack not in allpacks:
-				allpacks.append(curpack)
+            print("Changing to pack " + curpack)
+            continue
 
-			print("Changing to pack " + curpack)
-			continue
+        if curpack == None:
+            print("Addon pack name must be specified before filenames!")
+            break
 
-		if curpack == None:
-			print("Addon pack name must be specified before filenames!")
-			break
+        fpath = os.path.normpath(os.path.join(searchpath, finfo))
 
-		fpath = os.path.normpath(os.path.join(searchpath, finfo))
+        # Resolve all files that match the specified rule
+        print("Adding " + os.path.abspath(fpath) + " to pack " + curpack)
+        files = getFiles(fpath)
+        for f in files:
+            # ignore directories
+            if os.path.isdir(f):
+                continue
 
-		# Resolve all files that match the specified rule
-		print("Adding " + os.path.abspath(fpath) + " to pack " + curpack)
-		files = getFiles(fpath)
-		for f in files:
-			#ignore directories
-			if os.path.isdir(f):
-				continue
+            filemap[f] = curpack
 
-			filemap[f] = curpack
+    excluded = list(filter(lambda x: x == "ignore", allpacks))
+    for pack in excluded:
+        print('\tExcluding pack "' + pack + '"')
+        allpacks.remove(pack)
 
-	excluded = list(filter(lambda x : x == 'ignore', allpacks))
-	for pack in excluded:
-		print("\tExcluding pack \"" + pack + "\"")
-		allpacks.remove(pack)
-	
-	for pack in allpacks:
-		print("{0:<35}: {1}".format(pack, sizeof_fmt(getPackSize(pack, filemap))))
+    for pack in allpacks:
+        print(f"{pack:<35}: {sizeof_fmt(getPackSize(pack, filemap))}")
 
-	# Start building gmas for each addon
-	if args.build:
-		print("\n#### Beginning gma build ")
-		for pack in allpacks:
-			temp = tempfile.mkdtemp()
-			try:
-				buildGMA(pack, filemap, temp)
-			except Exception as err:
-				print("oops errored: ", err)
-				exit()
-			finally:
-				shutil.rmtree(temp)
+    # Start building gmas for each addon
+    if args.build:
+        print("\n#### Beginning gma build ")
+        for pack in allpacks:
+            temp = tempfile.mkdtemp()
+            try:
+                buildGMA(pack, filemap, temp)
+            except Exception as err:
+                print("oops errored: ", err)
+                exit()
+            finally:
+                shutil.rmtree(temp)
 
+    # Create or update the addon on workshop
+    if args.upload:
+        print("\n#### Beginning workshop upload ")
+        for pack in allpacks:
+            wsid = packids[pack]
+            try:
+                mode = "create"
+                extraparam = []
 
-	# Create or update the addon on workshop
-	if args.upload:
-		print("\n#### Beginning workshop upload ")
-		for pack in allpacks:
-			wsid = packids[pack]
-			try:
-				mode = "create" 
-				extraparam = []
+                if wsid != None:
+                    mode = "update"
+                    extraparam += ["-id", str(wsid)]
 
-				if wsid != None:
-					mode = "update"
-					extraparam += ["-id", str(wsid)]
+                if args.changelog != None:
+                    extraparam += ["-changes", args.changelog]
 
-				if args.changelog != None:
-					extraparam += ["-changes", args.changelog]
+                params = [
+                    gmpublish,
+                    mode,
+                    "-addon",
+                    getAddonName(pack),
+                    "-icon",
+                    "addon.jpg",
+                ] + extraparam
+                print("Uploading params:")
+                print(params)
 
-				params = [gmpublish, mode, "-addon", getAddonName(pack), "-icon", "addon.jpg"] + extraparam
-				print("Uploading params:")
-				print(params)
-
-				out = check_output(params, encoding='UTF-8')
-				print(out)
-			except CalledProcessError as e:
-				print("gmpublish FAILURE!!")
-				print(e.output)
+                out = check_output(params, encoding="UTF-8")
+                print(out)
+            except CalledProcessError as e:
+                print("gmpublish FAILURE!!")
+                print(e.output)
